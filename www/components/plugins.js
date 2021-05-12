09b782e63c7b79a5012f0b9250d90df53d0c2ebb
josejonathan7 <josejonathan689@gmail.com>
2021-05-13 02:45:36 +0900

    o plugin esta funcionando perfeitamente exceto a parte de salvar na galeria que esta gerando um erro no atributo saveToPhoto na parte de javascript

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,22 @@
 // This is a JavaScript file
+let elemento = document.querySelector("#camera");
 
+$(document).on("click", elemento, function(){
+  navigator.camera.getPicture(onSuccess, onFail, { 
+    quality: 50,
+    destinationType: 1,
+    MediaType: 1,
+    PictureSourceType: 1,
+    saveToPhotoAlbum: Camera.saveToPhotoAlbum.1
+     });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('imagem');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+});
